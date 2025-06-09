60d281dee779852c854a7018a63a7e461e94655c
Tatsu28 <joselynalexandraguerrarios@gmail.com>
2025-06-08 22:55:05 -0500

    Adaptando el Flask para Render

--- app.py
+++ app.py
@@ -227,5 +227,15 @@ def BuscarCartera():
 # ---------------------------------------------------------------------------
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    if not os.path.exists(app.config ['UPLOAD_FOLDER']):
+        os.makedirs(app.config [ 'UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+
+
+# if __name__ == "__main__":
+#    app.run(debug=True)
+
+
+
+
